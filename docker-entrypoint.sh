ed3c62980267bceacaf8313241f329e3fb4d23b2
Krzysztof Kardasz <krzysztof@kardasz.eu>
2017-11-02 23:12:43 +0100

    chmod

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
+# BasicAuth
 echo '' > /etc/nginx/basic_auth.conf
-
 if [ -n "$BASIC_AUTH" ]; then
     echo 'auth_basic "Restricted";' > /etc/nginx/basic_auth.conf
     echo 'auth_basic_user_file /etc/nginx/.htpasswd;' >> /etc/nginx/basic_auth.conf
     echo "$BASIC_AUTH" > /etc/nginx/.htpasswd
+    chmod 644 /etc/nginx/.htpasswd
 fi
 
 exec "$@"
