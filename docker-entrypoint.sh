e4382ef0da1fa7b7ab9db268c0eb2650308838e2
Krzysztof Kardasz <krzysztof@kardasz.eu>
2017-11-02 23:06:54 +0100

    Basic auth support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
+echo '' > /etc/nginx/basic_auth.conf
+
+if [ -n "$BASIC_AUTH" ]; then
+    echo 'auth_basic "Restricted";' > /etc/nginx/basic_auth.conf
+    echo 'auth_basic_user_file /etc/nginx/.htpasswd;' >> /etc/nginx/basic_auth.conf
+    echo "$BASIC_AUTH" > /etc/nginx/.htpasswd
+fi
+
 exec "$@"
